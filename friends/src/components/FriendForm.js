aa73fe2ebc70955a0b23c7c965574f39917bd2bd
Plofland <ploflan@gmail.com>
2021-01-27 00:31:19 -0500

    added the beginning of a friendform

--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -3,19 +3,34 @@
 // export default function FriendForm() {
 //   const [newFriend, setNewFriend] = useState(friends);
 
+//  const handleChange = () => {
+//    setNewFriend({
+//      ...friends,
+//      newfriend
+//    })
+//  }
+
+//  const submitFriend = () => {}
+
 //   return (
 //     <div>
 //       <form onSubmit={submitFriend}>
 //         <input
 //           type="text"
-//           name="username"
-//           value={credentials.username}
+//           name="name"
+//           value={}
 //           onChange={handleChange}
 //         />
 //         <input
-//           type="password"
-//           name="password"
-//           value={credentials.password}
+//           type="text"
+//           name="age"
+//           value={}
+//           onChange={handleChange}
+//         />
+//         <input
+//           type="text"
+//           name="email"
+//           value={}
 //           onChange={handleChange}
 //         />
 //         <button>Log In</button>
